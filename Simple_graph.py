a0a2be85948e538810a5f95f28eeaaeadccf302c
Coltarm <colin.treve@laposte.net>
2024-01-27 20:38:02 +0100

    Ajout de quelque fonction simple dans Ranfom_graph.py mais non tester

--- Simple_graph.py
+++ Simple_graph.py
@@ -13,16 +13,30 @@ class node:
 		estimate_time = self.time * value_gap/100
 		return estimate_time
 #Question comment on gère quands plusieurs noeuds font le choix en même temps, chacun ne doit pas être au courant du choix de l'autre
-def connection_choice(G, node): # On fait une fonction qui décrit le choix de à quelle noeuds décide de se connecter le noeuds, on donne en argument le graph et le noeuds qui fait le choix.
+def connection_choice(G, Nodes): # On fait une fonction qui décrit le choix que vont faire les noeuds à qui c'est leur tour de jouer
 	pass
 
 
 def create_graph(n)#créer un graphe de n noeuds
-	Node_liste = list(range(n))
+	Node_liste = [node(k) for k in range(n)] 
 	G = nx.Graph()
 	G.add_nodes_from(Node_Liste)
 	return G
 
-def who_play(Node_list, round): # objectif de cette fonction est de déterminer qui doit jouer au prochain tour. On prend en argument la liste des noeuds et le tour dans lequel on est.
-	smallest
-	pass
+def who_play(Node_list): # objectif de cette fonction est de déterminer qui doit jouer au prochain tour. On prend en argument la liste des noeuds qui doivent jouer et le tour dans lequel on est.
+	Node_smallest_time = [] 
+	smallest_time = 10000000000000000000000000
+	for node in Node_list:
+		if smallest_time<= node.getTime():
+			smallest = node.getTime()
+			Node_smallest_time.append(node)
+	return Node_smallest_time
+
+def game():
+	G = create_graph(10)
+	List_node = list(G.nodes)
+	List_nodes_to_play = List_node[:]
+	while Liste_nodes_to_play != []:
+		Playing_nodes = who_play(List_nodes_to_play)
+		connection_choice(G,Playing_nodes) # il faut compléter la fonction connection_choice pour créer la connection entre deux noeuds
+
